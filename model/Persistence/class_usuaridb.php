dfe4b1efea9a7a0761dbaec139b05edd2071dc68
Unknown <bruyah@hotmail.com>
2017-04-27 17:13:43 +0200

    login al 81%

--- model/Persistence/class_usuaridb.php
+++ model/Persistence/class_usuaridb.php
@@ -12,14 +12,31 @@ class usuaridb {
         $usuaris = array();
     }
 
-    public function consultarUsuarisdb() {
-        $query="SELECT id, username, password, descripcion FROM usuario;";
+    public function consultarUsuarisdb($name, $pass) {
+        $query="SELECT id, username, password, descripcion FROM usuario WHERE UserName= :user AND Password= :password;";
 		$con = new db();
-		$arrayDeUsuaris = $con->consultarUsuari($query);
-		$con->close();
 
-        $usuaris=$arrayDeUsuaris;
-		return $usuaris;
+        $resultado=$con->prepare($query);
+        $user = $name;
+        $password = $pass;
+        $resultado->bindValue(":user", $user);
+        $resultado->bindValue(":password", $password);
+        $resultado->execute();
+
+       $numero_registro=$resultado->rowCount(); //devuelve el número de registros que devuelve la consulta, 0 si no coincide el user y password y 1 si coincide
+//        if($numero_registro!=0){
+//            echo "USUARIO CORRECTO";
+//        }else{
+//
+//            header("location:index.php?ctl=usuari&act=login");
+//            echo "USUARIO NO ENCONTRADO";
+//        }
+
+//		$arrayDeUsuaris = $con->consultarUsuari($query);
+		$con = null;
+
+//        $usuaris=$arrayDeUsuaris;
+		return $numero_registro;
                 
     }
 }
