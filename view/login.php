aad860c70b6600219632dd686b49eb51c3c7976c
Unknown <javi.iglesias.valencia@gmail.com>
2017-04-30 12:06:50 +0200

    Refactorización Login

--- view/login.php
+++ view/login.php
@@ -1,8 +1,7 @@
 <?php
-
-$guardarUsuari = "";
-if (isset($_COOKIE['usuari'])) {
-    $guardarUsuari = $_COOKIE['usuari'];
+$guardarUsuario = "";
+if (isset($_COOKIE['usuario'])) {
+    $guardarUsuario = $_COOKIE['usuario'];
 }
 ?>
 <html>
@@ -10,24 +9,25 @@ if (isset($_COOKIE['usuari'])) {
 <div class="container-fluid">
     <div class="container">
         <div class="col-xs-8 col-sm-8  col-md-4 col-xs-offset-3 col-sm-offset-4 col-md-offset-4">
-            <img src="view/images/logo.png" class="img-responsive img-Logologin">
+
         </div>
         <div class="col-xs-11 col-md-4 col-xs-offset-1 col-md-offset-4 ">
             <div class="container-login">
                 <form action="?ctl=login" method="POST" class="login-form">
-                    <h1 class="text-center transparent-heading">Iniciar Sessió</h1>
+                    <img src="view/images/original2.png" class="imagen-login">
+                    <h1 class="text-center transparent-heading">Iniciar Sesión</h1>
                     <div class="form-group">
-                        <label><strong>Usuari:</strong></label>
-                        <input type="text" name="usuari" value="<?php echo $guardarUsuari; ?>" class="form-control">
+                        <label><strong>Usuario:</strong></label>
+                        <input type="text" name="usuario" value="<?php echo $guardarUsuario; ?>" class="form-control">
                     </div>
                     <div class="form-group">
-                        <label><strong>Contrasenya:</strong></label>
+                        <label><strong>Contraseña:</strong></label>
                         <input type="password" name="pass" class="form-control">
                     </div>
-                    <p>Recordar usuari <input type="checkbox" name="recordarUsuari"><p>
-                    <p><a href="?ctl=usuari&act=registre">Registrar-se com a usuari</a></p>
+                    <p>Recordar usuario <input type="checkbox" name="recordarUsuario"><p>
+                    <p><a href="?ctl=usuari&act=registre">Registrarse como  usuario</a></p>
                     <div class="col-xs-10 col-md-9 col-md-offset-3 col-xs-offset-2">
-                        <button name="Submit" class="btn btn-warning"> Entrar <image class="btn-icon" src="view/images/login.ico"/> </button>
+                        <button name="Submit" class="btn btn-warning"> Entrar <image class="btn-icon imagen-button-login"  src="view/images/login.ico"/> </button>
                     </div>
                     <form>
             </div>
