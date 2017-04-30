68a359eb35a605496294ba803b1b48088bf36f27
Unknown <acasasfilella@gmail.com>
2017-04-30 14:01:12 +0200

    mejoras en maquetación inicio y login
    falta por mejorar el footer ( da problemas)

--- view/inicio.php
+++ view/inicio.php
@@ -1,96 +1,65 @@
 <?php
 ?>
 
-<div class="w100">
+<div class="w100 topflexslide">
   <div class="flexslider">
   <ul class="slides">
-    <li>
-      <img class="img-responsive" src="view/images/ejercicio1.jpg" />
+    <li class="bgSlide1">
+     <!-- <img class="img-responsive" src="view/images/ejercicio1.jpg" />-->
     </li>
-    <li>
-      <img class="img-responsive" src="view/images/ejercicio2.jpg" />
+    <li class="bgSlide2">
+      <!--<img class="img-responsive" src="view/images/ejercicio2.jpg" />-->
     </li>
-    <li>
-      <img class="img-responsive" src="view/images/ejercicio3.jpg" />
+    <li class="bgSlide3">
+      <!--<img class="img-responsive" src="view/images/ejercicio3.jpg" />-->
     </li>
-    <li>
-      <img class="img-responsive" src="view/images/ejercicio4.jpg" />
+    <li class="bgSlide4">
+      <!--<img class="img-responsive" src="view/images/ejercicio4.jpg" />-->
     </li>
-     <li>
-      <img class="img-responsive" src="view/images/fitness.jpg" />
+     <li class="bgSlide5">
+     <!-- <img class="img-responsive" src="view/images/fitness.jpg" />-->
     </li>
   </ul>
 </div>
-
-
+</div>
+<div class="clear mb614"></div>
 
 <div class="w100">
-  <div class="tith2">
+<div class="container mB90 text-center">
     <h2>Últimes notícies</h2>
-  </div>
-</div>
-
-<div class="container mB90 ">
 
   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="">
-     
-      <a href="http://www.ascensors-serra.com/ca/noticies/-2/unio-esportiva-olot-8.htm" class="bgCover3" style=" background-image: url('http://www.ascensors-serra.com/img/galeria/temporada_2016-17-serra-71.jpg'); " >
-      </a>  
+    <div class="recuadro">
+     <img class="img-responsive" src="view/images/fitness.jpg" />
+          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 1</h3>
+      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
+      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
     </div>
-    <div class="">
-      <a href="http://www.ascensors-serra.com/ca/noticies/-2/unio-esportiva-olot-8.htm">
-      <h3 class="bold colorRed pT20 dInlineB fz18 h60">UNIÓ ESPORTIVA OLOT</h3>
-      <span class="lite pT20 dInlineB h70">Foto del primer equip de la Uni&oacute; esportiva Olot. Objectiu ascens a 2ona.</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">Llegir més</span></a>
-    </div>
-
   </div>
-  
   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="">
-     
-      <a href="http://www.ascensors-serra.com/ca/noticies/-2/trailwalker-2017-7.htm" class="bgCover3" style=" background-image: url('http://www.ascensors-serra.com/img/galeria/img_7869-70.jpg'); " >
-      </a>  
+    <div class="recuadro">
+     <img class="img-responsive" src="view/images/fitness.jpg" />
+          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 2</h3>
+      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
+      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
     </div>
-    <div class="">
-      <a href="http://www.ascensors-serra.com/ca/noticies/-2/trailwalker-2017-7.htm">
-      <h3 class="bold colorRed pT20 dInlineB fz18 h60">Trailwalker 2017</h3>
-      <span class="lite pT20 dInlineB h70">Felicitats a l'equip d'ASCENSORS SERRA, per haver finalitzat amb &egrave;xit la OXFAM TRAILWALKER 2017</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">Llegir més</span></a>
+  </div>
+   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
+    <div class="recuadro">
+     <img class="img-responsive" src="view/images/fitness.jpg" />
+          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 3</h3>
+      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
+      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
     </div>
-
   </div>
   
-  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="">
-     
-      <a href="http://www.ascensors-serra.com/ca/noticies/-3/ascensors-serra-nou-patrocinador-unio-esportiva-olot-6.htm" class="bgCover3" style=" background-image: url('http://www.ascensors-serra.com/img/galeria/1-56.jpg'); " >
-      </a>  
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
+    <div class="recuadro">
+     <img class="img-responsive" src="view/images/fitness.jpg" />
+          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 4</h3>
+      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
+      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
     </div>
-    <div class="">
-      <a href="http://www.ascensors-serra.com/ca/noticies/-3/ascensors-serra-nou-patrocinador-unio-esportiva-olot-6.htm">
-      <h3 class="bold colorRed pT20 dInlineB fz18 h60">Ascensors Serra, nou patrocinador de la Unió Esportiva Olot</h3>
-      <span class="lite pT20 dInlineB h70">L&rsquo;empresa olotina Ascensors Serra es converteix en el patrocinador m&eacute;s important de la Uni&oacute; Esportiva Olot. Es tracta d&rsquo;una...</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">Llegir més</span></a>
-    </div>
-
-  </div>
-  
-  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="">
-     
-      <a href="http://www.ascensors-serra.com/ca/noticies/-2/accessibilitat-5.htm" class="bgCover3" style=" background-image: url('http://www.ascensors-serra.com/img/galeria/1-57.jpg'); " >
-      </a>  
-    </div>
-    <div class="">
-      <a href="http://www.ascensors-serra.com/ca/noticies/-2/accessibilitat-5.htm">
-      <h3 class="bold colorRed pT20 dInlineB fz18 h60">Accessibilitat </h3>
-      <span class="lite pT20 dInlineB h70">Faci la seva vida m&eacute;s f&agrave;cil amb una cadira pujaescales&nbsp;amb seient giratori.
-&nbsp;</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">Llegir més</span></a>
-    </div>
-
   </div>
   </div>
 
