4c83cebf3b11e5511567e5da2f4e8497be68d144
Unknown <acasasfilella@gmail.com>
2017-05-03 17:59:56 +0200

    noticias detalle y pagina 404

---
+++ view/404.php
@@ -0,0 +1,8 @@
+
+<div class="container pRelative " id="main">
+	<div class="col-md-6 col-md-offset-3 col-xs-12 col-xs-offset-0">
+		<h1> ERROR 404 Pagina no encontrada</h1>
+		<span>La página que estás buscando no se ha encontrado. Intenta volver a la página inicial y buscar lo que te interese.</span>
+	</div>
+	
+</div>
