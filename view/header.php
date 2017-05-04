ac7879c74a754b35541acdd9d2adfa902d3c98e7
Unknown <acasasfilella@gmail.com>
2017-05-04 18:00:01 +0200

    arreglados algunoes errores de maquetacion

--- view/header.php
+++ view/header.php
@@ -3,21 +3,28 @@
 <head>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   <script src="view/js/test.js"></script>
 
 <script defer src="view/js/jquery.flexslider-min.js"></script>
 <script defer src="view/js/jquery.flexslider-min.js"></script>
+
+
+<link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah|Josefin+Sans|Muli" rel="stylesheet">
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="view/js/test.js"></script>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <link href="view/css/flexslider.css" rel="stylesheet" type="text/css">
 <link href="view/css/estils.css" rel="stylesheet" type="text/css">
-    <link href="view/css/prueba.css" rel="stylesheet" type="text/css">
+<link href="view/css/prueba.css" rel="stylesheet" type="text/css">
 
     <title>Guia de continguts</title>
 </head>
--- view/noticiaDetalle.php
+++ view/noticiaDetalle.php
@@ -7,7 +7,7 @@
 		<h1> <?echo $noticias->getTitulo()?></h1>
 		</div>
 		<div class="col-xs-12 col-sm-4 text-center">
-		<img  class="img-responsive " src="view/images/<?echo $noticias->getImagen();?>"></div>
+		<img  class="img-responsive " src='view/images/<?echo $noticias->getImagen();?>'></div>
 		<div class="col-xs-12 col-sm-8"><span ><? echo $noticias->getContenido();?></span></div>
 
 	 <?endforeach ?>
