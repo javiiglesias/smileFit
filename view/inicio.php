928f089dd585fe1dac6ecc545fe7bc7fe3ee585a
Unknown <marc.jimenez.delgado@hotmail.com>
2017-05-04 20:04:34 +0200

    try

--- view/inicio.php
+++ view/inicio.php
@@ -8,8 +8,25 @@
 
 <div class="w100">
 <div class="container mB90 text-center">
-    <h2>Últimes notícies</h2>
+    <h2>Últimas noticias</h2>
+</div>
+<!-- <div class="container mB90 ">
+  <?php for($i=0;$i<3;$i++) ?>
+  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
+    <div class="">
+     
+      <a href="?ctl=noticias&act=detalle&param=<?php echo $noticias->getId();?>" class="bgCover3" style=" background-image: url('view/images/<?echo $noticias->getImagen();?>'); " >
+      </a>  
+    </div>
+    <div class="">
+      <a href="?ctl=noticias&act=detalle&param=<?php echo $noticias->getId();?>">
+      <h3 class="bold colorRed pT20 dInlineB fz18 h60"><?echo $noticias->getTitulo();?></h3>
+      <span class="lite pT20 dInlineB h70"><? echo $noticias->getDescripcion();?></span>
+      <span class="pT20 pB20xs dInlineB normal fz14">Llegir més</span></a>
+    </div>
 
+  </div>
+</div> -->
   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
     <div class="recuadro">
      <img class="img-responsive" src="view/images/fitness.jpg" />
