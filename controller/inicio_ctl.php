eb54aaa02aa16ff02f779cab2d6097394b2ec70c
Unknown <acasasfilella@gmail.com>
2017-05-05 16:34:55 +0200

    noticas al inicio

--- controller/inicio_ctl.php
+++ controller/inicio_ctl.php
@@ -2,7 +2,16 @@
 $titlePage = "Inicio";
 
 require_once 'view/header.php';
+require_once("controller/function_AutoLoad.php");
+
+	$noticias = new noticia();
+
+
+    $noticia = $noticias->muestraNoticia();
+
+
+
 require_once 'view/inicio.php';
 require_once 'view/footer.php';
-
+ob_end_flush();
 ?>
--- view/inicio.php
+++ view/inicio.php
@@ -9,61 +9,20 @@
 <div class="w100">
 <div class="container mB90 text-center">
     <h2>Últimas noticias</h2>
+
+<?php foreach ($noticia as $noticias ): ?>
+  <?var_dump($noticias)?>
+  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
+    <div class="recuadro">
+     <img class="img-responsive" src="view/images/<? echo $noticias->getImagen();?>" />
+          <h3 class="bold colorGrey pT20 dInlineB fz18 h60"><?echo $noticias->getTitulo();?></h3>
+      <span class="lite pT20 dInlineB h70"><?echo $noticias->getDescripcion();?></span>
+      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
+    </div>
+  </div>
+<?php endforeach ?>
+
+  </div>
+
+
 </div>
-<!-- <div class="container mB90 ">
-  <?php for($i=0;$i<3;$i++) ?>
-  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="">
-     
-      <a href="?ctl=noticias&act=detalle&param=<?php echo $noticias->getId();?>" class="bgCover3" style=" background-image: url('view/images/<?echo $noticias->getImagen();?>'); " >
-      </a>  
-    </div>
-    <div class="">
-      <a href="?ctl=noticias&act=detalle&param=<?php echo $noticias->getId();?>">
-      <h3 class="bold colorRed pT20 dInlineB fz18 h60"><?echo $noticias->getTitulo();?></h3>
-      <span class="lite pT20 dInlineB h70"><? echo $noticias->getDescripcion();?></span>
-      <span class="pT20 pB20xs dInlineB normal fz14">Llegir més</span></a>
-    </div>
-
-  </div>
-</div> -->
-  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="recuadro">
-     <img class="img-responsive" src="view/images/fitness.jpg" />
-          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 1</h3>
-      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
-    </div>
-  </div>
-  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="recuadro">
-     <img class="img-responsive" src="view/images/fitness.jpg" />
-          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 2</h3>
-      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
-    </div>
-  </div>
-   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="recuadro">
-     <img class="img-responsive" src="view/images/fitness.jpg" />
-          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 3</h3>
-      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
-    </div>
-  </div>
-  
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 mB15xs ">
-    <div class="recuadro">
-     <img class="img-responsive" src="view/images/fitness.jpg" />
-          <h3 class="bold colorGrey pT20 dInlineB fz18 h60">Noticia 4</h3>
-      <span class="lite pT20 dInlineB h70">Descripcion noticia 1</span>
-      <span class="pT20 pB20xs dInlineB normal fz14">leer mas</span>
-    </div>
-  </div>
-  </div>
-
-
-
-
-<?php
-?>
