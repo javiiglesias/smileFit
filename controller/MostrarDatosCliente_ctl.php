e00c9067a07bf4ec4077074ee7b06c72f72d36fa
Unknown <javi.iglesias.valencia@gmail.com>
2017-05-07 00:10:20 +0200

    Modificaciones Menus/Creacion Cliente

---
+++ controller/MostrarDatosCliente_ctl.php
@@ -0,0 +1,7 @@
+<?php
+ob_start();
+require_once("controller/function_AutoLoad.php");
+$titlePage = " ";
+$Usuarios = new usuario();
+require_once 'view/header.php';
+
--- controller/menuCliente_ctl.php
+++ controller/menuCliente_ctl.php
@@ -1,12 +1,11 @@
 <?php
 ob_start();
-
 $user=  $_SESSION["usuario"] ;
 $titlePage = "Dashboard de ".$user;
 
-require_once 'view/header.php';
-require_once 'view/menuCliente.php';
-require_once 'view/footer.php';
 
+require_once 'view/header.php';
+require_once 'view/menuAdministrador.php';
+require_once 'view/footer.php';
 ob_end_flush();
 ?>
