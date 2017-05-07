42c3c9985c9c12c7e82fc6620e130178a5150296
Unknown <acasasfilella@gmail.com>
2017-05-07 11:43:21 +0200

    sol bug muestra cliente cuando no estas logeado como cliente

--- controller/menuCliente_ctl.php
+++ controller/menuCliente_ctl.php
@@ -1,10 +1,16 @@
 <?php
 ob_start();
-$user=  $_SESSION["usuario"] ;
+if( $_SESSION["usuario"]){
+	$user=  $_SESSION["usuario"];
 $titlePage = "Dashboard de ".$user;
 
 require_once 'view/header.php';
 require_once 'view/menuCliente.php';
 require_once 'view/footer.php';
+}else{
+	header('location:view/inicio.php');
+}
+
+
 ob_end_flush();
 ?>
