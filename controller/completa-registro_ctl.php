38d0e5a453998d18585410152bacc3584e315e04
Unknown <acasasfilella@gmail.com>
2017-05-10 18:28:28 +0200

    alta trabajador v2

--- controller/completa-registro_ctl.php
+++ controller/completa-registro_ctl.php
@@ -3,8 +3,20 @@ require_once("controller/function_AutoLoad.php");
 
 $trabajador= new trabajador();
 
-$worker= $trabajador->addWorker($nombre,$apellidos,$fechaNacimiento,$email,$telefono,$foto);
-echo"hola";
-die();
+require_once 'view/header.php';
+if (isset($_REQUEST['Submit'])) {
+    if (isset($_REQUEST['nombre'])) {
+		$worker= $trabajador->addWorker($_REQUEST['nombre'],$_REQUEST['apellidos'],$_REQUEST['fechaNacimiento'],$_REQUEST['email'],$_REQUEST['telefono'],$_REQUEST['foto'],$_REQUEST['rol']);
+
+		var_dump($worker);
+		die();
+		$mensaje = "Te has registrado correctamente, Puedes iniciar Session";
+        require_once 'view/confirmacion.php';
+		}else{
+
+		}
+	}else{
+
+	}
 
 ?>
