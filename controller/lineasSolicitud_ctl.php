e56bbf2c1bab0ccc0e22a4fcf7c6100aff670bc6
Unknown <acasasfilella@gmail.com>
2017-05-20 18:04:47 +0200

    solicitud-> linea solicitud indica nombre quien la ha escrito

--- controller/lineasSolicitud_ctl.php
+++ controller/lineasSolicitud_ctl.php
@@ -8,71 +8,57 @@ require_once 'view/header.php';
 
 $usuarios = new usuario();
 
-$idSolicitud=$_REQUEST['param'];
+$idSolicitud = $_REQUEST['param'];
 
 $asignado = false;
 
-if(isset($_SESSION["test2"])){
-   $idUser= $_SESSION["test2"];
+if (isset($_SESSION["test2"])) {
+    $idUser = $_SESSION["test2"];
 }
 
 //obtenemos el usuario actual
 $usuarioRol = $usuarios->getUserPorId($idUser);
 
 //si es trabajador
-if($usuarioRol == 'Trabajador')
-{
-	//buscar idTrabajador para buscar sus solicitudes
-	$trabajadores = new trabajador();
-	$idTrabajador = $trabajadores->getTrabajadorPorIdUser($idUser);	
-	$trabajador = $trabajadores->getTrabajador($idUser);	
+if ($usuarioRol == 'Trabajador') {
+    //buscar idTrabajador para buscar sus solicitudes
+    $trabajadores = new trabajador();
+    $idTrabajador = $trabajadores->getTrabajadorPorIdUser($idUser);
+    $trabajador = $trabajadores->getTrabajador($idUser);
 
-	//obtener titulo solicitud
-	$solicitud = new solicitud();
-	$tit = $solicitud->getTituloSolicitud($idSolicitud);
+    //obtener titulo solicitud
+    $solicitud = new solicitud();
+    $tit = $solicitud->getTituloSolicitud($idSolicitud);
 
-	$key = array_values($tit[0]); 
+    $key = array_values($tit[0]);
     $titulo = $key[0];
-	//mostrar lineas solicitud
-	$lineasSolicitudes = new lineasolicitud();
-	$lineasSolicitud = $lineasSolicitudes->muestraLineasPorIdSolicitud($idSolicitud);
+    //mostrar lineas solicitud
+    $lineasSolicitudes = new lineasolicitud();
+    $lineasSolicitud = $lineasSolicitudes->muestraLineasPorIdSolicitud($idSolicitud);
+} else if ($usuarioRol == 'Cliente' || $usuarioRol == 'cliente') {
+    //si es cliente
+    //buscar idCliente para buscar sus solicitudes
+    $clientes = new cliente();
+    $idCliente = $clientes->getClientePorIdUser($idUser);
+    $cliente = $clientes->getCliente($idUser);
 
+    //obtener titulo solicitud
+    $solicitud = new solicitud();
+    $tit = $solicitud->getTituloSolicitud($idSolicitud);
 
-}
-else if($usuarioRol == 'Cliente' || $usuarioRol == 'cliente'){
-	//si es cliente
-	//buscar idCliente para buscar sus solicitudes
-	$clientes = new cliente();
-	$idCliente = $clientes->getClientePorIdUser($idUser);	
-	$cliente = $clientes->getCliente($idUser);	
-
-	//obtener titulo solicitud
-	$solicitud = new solicitud();
-	$tit = $solicitud->getTituloSolicitud($idSolicitud);
-
-	$key = array_values($tit[0]); 
+    $key = array_values($tit[0]);
     $titulo = $key[0];
 
-	//mostrar lineas solicitud
-	$lineasSolicitudes = new lineasolicitud();
-	$lineasSolicitud = $lineasSolicitudes->muestraLineasPorIdSolicitud($idSolicitud);
-	
+    //mostrar lineas solicitud
+    $lineasSolicitudes = new lineasolicitud();
+    $lineasSolicitud = $lineasSolicitudes->muestraLineasPorIdSolicitud($idSolicitud);
 }
 
 $idClienteSolicitador = $lineasSolicitud[0]->getIdCliente();
 
-$trabajadores = new trabajador();
-$tr = $trabajadores->getTrabajadores();
-//var_dump($tr);
-
-$clientes = new cliente();
-$cl = $clientes->getCustomers();
-//var_dump($cl);
-
-
-if($_GET['act']=='mostrarLineasSolicitud'){
-	require_once 'view/lineasSolicitud.php';
-        require_once 'view/footer.php';
+if ($_GET['act'] == 'mostrarLineasSolicitud') {
+    require_once 'view/lineasSolicitud.php';
+    require_once 'view/footer.php';
 }
 
 ob_end_flush();
