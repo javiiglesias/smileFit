11336daf5dbb91112a9a58f7dbc7456b4c2ba152
Unknown <marc.jimenez.delgado@hotmail.com>
2017-05-20 21:30:22 +0200

    maquetacion solicitudes asignadas

--- view/mostrarSolicitudesAsignadas.php
+++ view/mostrarSolicitudesAsignadas.php
@@ -11,9 +11,9 @@
             $nombreT = $trabajadores->getNombreApellidos($idT);
             $nombreC = $clientes->getNombreApellidos($idC);
             $nombreS = $solicitudes->GetSolicitudDescripcion($idS); ?>
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mT20 borderGris pL0">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mT35 borderGris pL0">
      
-            <div class="col-xs-12 col-md-9 col-sm-6 col-lg-8pB35 h115 mB15">
+            <div class="col-xs-12 col-md-9 col-sm-6 col-lg-8pB35 h175 mB15">
                 <h2><?php echo $entrenamientos->getDescripcion(); ?></h2>
                 <span class="lite fz13"><b>Fecha Inicio: </b> <?php echo $entrenamientos->getFechaInicio(); ?></span>
                 <span class="lite fz13"><b>&nbsp; Fecha Fin: </b><?php echo $entrenamientos->getFechaFin(); ?></span><br>
@@ -24,7 +24,8 @@
         </div>
         <?php endforeach;
      
-    }else{
+    }else{ 
+
         foreach ($entrenamiento as $entrenamientos): 
             $idC = $entrenamientos->getIdCliente();
             $idT = $entrenamientos->getIdTrabajador();
@@ -34,8 +35,8 @@
             $nombreC = $clientes->getNombreApellidos($idC);
             $nombreS = $solicitudes->GetSolicitudDescripcion($idS); ?>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mT20 borderGris pL0">
-     
-                <div class="col-xs-6 col-md-5 col-sm-3 col-lg-8pB35 h115 mB15">
+                
+                <div class="col-xs-6 col-md-5 col-sm-3 col-lg-8pB35 h175 mB15">
                     <h2><?php echo $entrenamientos->getDescripcion();?></h2>
                     <span class="lite fz13"><b>Fecha Inicio: </b> <?php echo $entrenamientos->getFechaInicio();?></span>
                     <span class="lite fz13"><b>&nbsp; Fecha Fin: </b><?php echo $entrenamientos->getFechaFin();?></span><br>
@@ -55,16 +56,16 @@
             $nombreC = $clientes->getNombreApellidos($idC);
             $nombreS = $solicitudes->GetSolicitudDescripcion($idS); ?>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 mT20 borderGris pL0">
-     
-            <div class="col-xs-6 col-md-5 col-sm-3 col-lg-8pB35 h115 mB15">
-                <h2><?php echo $dietas->getDescripcion();?></h2>
-                <span class="lite fz13"><b>Fecha Inicio: </b> <?php echo $dietas->getFechaInicio();?></span>
-                <span class="lite fz13"><b>&nbsp; Fecha Fin: </b><?php echo $dietas->getFechaFin();?></span><br>
-                <span class="lite fz13"><b>Cliente: </b><?php echo $nombreC; ?></span>
-                <span class="lite fz13"><b>&nbsp; Trabajador: </b><?php echo $nombreT; ?></span><br>
-                <span class="lite fz13"><b>Solicitud: </b><?php echo $nombreS; ?></span>
+                
+                <div class="col-xs-6 col-md-5 col-sm-3 col-lg-8pB35 h175 mB15">
+                    <h2><?php echo $dietas->getDescripcion();?></h2>
+                    <span class="lite fz13"><b>Fecha Inicio: </b> <?php echo $dietas->getFechaInicio();?></span>
+                    <span class="lite fz13"><b>&nbsp; Fecha Fin: </b><?php echo $dietas->getFechaFin();?></span><br>
+                    <span class="lite fz13"><b>Cliente: </b><?php echo $nombreC; ?></span>
+                    <span class="lite fz13"><b>&nbsp; Trabajador: </b><?php echo $nombreT; ?></span><br>
+                    <span class="lite fz13"><b>Solicitud: </b><?php echo $nombreS; ?></span>
+                </div>
             </div>
-        </div>
         <?php endforeach;
     }?>
 </div>
