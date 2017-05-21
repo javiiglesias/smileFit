f8b936003d19650d0a1c6d904c2b19666d6b5197
Unknown <marc.jimenez.delgado@hotmail.com>
2017-05-21 14:14:24 +0200

    calendar parte 2

--- controller/mostrarAgenda_ctl.php
+++ controller/mostrarAgenda_ctl.php
@@ -1,10 +1,10 @@
 <?php
 //ob_start();
 //require_once("controller/function_AutoLoad.php");
-//$titlePage = "Mi Agenda";
+$titlePage = "Mi Agenda";
 //
 //require_once 'view/header.php';
-////require_once 'view/mostrarAgenda.php';
+require_once 'view/mostrarAgenda.php';
 ////require_once 'view/partials/header-calendar.php';
 //
 ////$usuarios = new usuario();
@@ -59,6 +59,6 @@
 //// 	);
 //// }
 //
-////require_once 'view/footer.php';
+require_once 'view/footer.php';
 //ob_end_flush();
 //?>
