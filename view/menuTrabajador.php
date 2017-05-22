b00b5139c6c32b768c4e8cdd2bf34b698bf8df8f
Unknown <marc.jimenez.delgado@hotmail.com>
2017-05-22 17:05:05 +0200

    test2

--- view/menuTrabajador.php
+++ view/menuTrabajador.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <div id="wrapper">
     <div class="overlay"></div>
 
@@ -24,37 +24,4 @@
             </div>
         </div>
     </div>
-=======
-<div id="wrapper">
-    <div class="overlay"></div>
-
-    <?php require_once("partials/navigationTrabajador.php"); ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 text-center">
-
-            </div>
-            <div class="col-xs-12 col-md-6">
-                <h1>Info Trabajador</h1>
-                <!--                <ul>
-                                 <li><a href="?ctl=trabajador&act=VerDatosClientes">Ver Ficha Personal Clientes</a></li> 
-                                <li><a href="?ctl=trabajador&act=MostrarDatos">Visualizar Mis Datos</a></li>
-                                <li><a href="?ctl=trabajador&act=mostrarSolicitudes">Mostrar Solicitudes Pendientes</a></li>
-                                <li><a href="?ctl=trabajador&act=mostrarSolicitudesAsignadas">Mostrar Solicitudes En Curso</a></li>
-                                <li><a href="?ctl=trabajador&act=mostrarAgenda">Visualizar Mi Agenda</a></li>
-                                </ul>-->
-                <?php require_once('partials/contentNews.php'); ?>
-            </div>
-            <div class="col-xs-12 col-md-6 mB15">
-                <h1>Solicitudes Asignadas.</h1>
-                <?php require_once 'view/partials/solicitudesAsignadas.php'; ?>
-            </div>
-             <div class="col-xs-12 col-md-offset-3 col-xs-offset-0 col-md-6 mT20 text-center">
-    
-                <h1>Solicitudes Pendientes.</h1>
-                <?php require_once 'view/partials/solicitudesPendientes.php'; ?>
-            </div>
-        </div>
     </div>
->>>>>>> Marcos
-</div>
